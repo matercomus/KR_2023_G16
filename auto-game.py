5bf5064d8d31f674a5da8092a09c2cb372425cf8
matercomus <matek.kedzia@gmail.com>
2023-12-07 02:11:59 +0100

    refactor

--- auto-game.py
+++ auto-game.py
@@ -1,63 +1,58 @@
 import json
-from game import Game  # Import the Game class from the game module
+import random
+from game import Game
 
 
-def choose_proponent_move(game, options):
-    """
-    Choose the option with the highest difference
-    between the number of successors and predecessors
-    """
-    options.sort(
-        key=lambda option: len(list(game.G.successors(option)))
-        - len(list(game.G.predecessors(option))),
-        reverse=True,
+class AutoGame:
+    def __init__(self, n_games, claimed_argument, data_file, save_graph_dir):
+        self.n_games = n_games
+        self.claimed_argument = claimed_argument
+        self.data_file = data_file
+        self.save_graph_dir = save_graph_dir
+        self.results = []
+
+    def choose_proponent_move(self, game, options):
+        options.sort(
+            key=lambda option: len(list(game.G.successors(option)))
+            - len(list(game.G.predecessors(option))),
+            reverse=True,
+        )
+        return options[0]
+
+    def choose_opponent_move(self, _, options):
+        return random.choice(options)
+
+    def play_games(self):
+        for _ in range(self.n_games):
+            game = Game(
+                claimed_argument=self.claimed_argument,
+                data_file=self.data_file,
+                verbose=False,
+                save_graph_dir=self.save_graph_dir,
+                add_game_text=True,
+                choose_proponent_move=self.choose_proponent_move,
+                choose_opponent_move=self.choose_opponent_move,
+            )
+            game.play()
+            self.results.append(
+                {
+                    "id": str(game.id),
+                    "proponent_arguments": game.proponent_arguments,
+                    "opponent_arguments": game.opponent_arguments,
+                }
+            )
+
+    def save_results(self):
+        with open("games_results.json", "w") as f:
+            json.dump(self.results, f)
+
+
+if __name__ == "__main__":
+    auto_game = AutoGame(
+        n_games=10,
+        claimed_argument="0",
+        data_file="Argumentation_Framework_tests/example-argumentation-framework.json",
+        save_graph_dir="test_dir",
     )
-    return options[0]
-
-
-def choose_opponent_move(game, options):
-    """
-    Choose the option with the highest number of successors
-    """
-    options.sort(key=lambda option: len(list(game.G.successors(option))), reverse=True)
-    return options[0]
-
-
-def play_games(
-    n,
-    data_file,
-    claimed_argument,
-    verbose=False,
-    show_graph=False,
-    save_graph=False,
-    add_game_text=False,
-):
-    results = []
-    for i in range(n):
-        game = Game(
-            data_file,
-            claimed_argument,
-            verbose,
-            show_graph,
-            save_graph,
-            add_game_text,
-            choose_proponent_move,
-            choose_opponent_move,
-        )
-        game.play()
-        results.append(
-            {
-                "proponent_arguments": game.proponent_arguments,
-                "opponent_arguments": game.opponent_arguments,
-                "winner": "proponent"
-                if len(game.opponent_arguments) == 0
-                else "opponent",
-            }
-        )
-
-    with open("results.json", "w") as f:
-        json.dump(results, f)
-
-
-# Call the play_games function to play n games and save the results
-play_games(10, "path_to_your_data_file", "your_claimed_argument")
+    auto_game.play_games()
+    auto_game.save_results()
